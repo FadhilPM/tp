18f26c16c4dc5e63a8e3d89b6b308dd212b92c3f
Cheng Jing <chengjing32@yahoo.com>
2025-03-10 11:09:36 +0800

    Update message Usage for "add" command

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -26,7 +26,8 @@ public class AddCommand extends Command {
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
-            + PREFIX_TAG + "friends "; //+ PREFIX_TAG + "owesMoney";
+            + PREFIX_TAG + "friends "
+            + PREFIX_TAG + "owesMoney";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
