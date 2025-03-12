0a73b03daeae79699a92de0c9add896ef4aaa582
broccoli-hater <ihatebroccoli404@gmail.com>
2025-03-12 22:41:28 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,10 +1,10 @@
 package seedu.address.logic.parser;
 
-import seedu.address.commons.util.StringUtil;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
 
+import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
