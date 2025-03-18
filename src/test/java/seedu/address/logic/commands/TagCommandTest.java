0140c83eb56b6b82a1cf2b55d610563f5f999b5a
Kennethsoh <sgguy26@gmail.com>
2025-03-18 13:27:25 +0800

    Add Command tests

---
+++ src/test/java/seedu/address/logic/commands/TagCommandTest.java
@@ -0,0 +1,42 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.TagCommand.tagProjectToPerson;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for TagCommand.
+ */
+public class TagCommandTest {
+
+    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_Tag_success() {
+        Person personToTag = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Phone phone = personToTag.getPhone();
+        Tag newTag = new Tag("T_3st-x");
+        Set<Tag> tags = new LinkedHashSet<>();
+        tags.add(newTag);
+        TagCommand tagComd = new TagCommand(phone, tags);
+        Person taggedPerson = tagProjectToPerson(personToTag, tags);
+
+        String expectedMessage = String.format(TagCommand.MESSAGE_SUCCESS);
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(personToTag, taggedPerson);
+
+        assertCommandSuccess(tagComd, model, expectedMessage, expectedModel);
+    }
+}
---
+++ src/test/java/seedu/address/logic/commands/UnTagCommandTest.java
@@ -0,0 +1,44 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.TagCommand.tagProjectToPerson;
+import static seedu.address.logic.commands.UnTagCommand.unTagProjectFromPerson;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for TagCommand.
+ */
+public class UnTagCommandTest {
+
+    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_UnTag_success() {
+        Person personToTag = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Phone phone = personToTag.getPhone();
+        Tag newTag = new Tag("T_3st-x");
+        Set<Tag> tags = new LinkedHashSet<>();
+        tags.add(newTag);
+        UnTagCommand untagComd = new UnTagCommand(phone, tags);
+        Person unTaggedPerson = unTagProjectFromPerson(personToTag, tags);
+
+        String expectedMessage = String.format(UnTagCommand.MESSAGE_SUCCESS);
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(personToTag, unTaggedPerson);
+
+        assertCommandSuccess(untagComd, model, expectedMessage, expectedModel);
+
+    }
+}
