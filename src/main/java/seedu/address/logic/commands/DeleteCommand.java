11bced409c1b95d937f32b14637229404b9c1426
Fadhil <e0148630@u.nus.edu>
2025-03-19 11:37:40 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -15,9 +15,8 @@ public abstract class DeleteCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD
         + ": Deletes a person from the address book. "
         + "Parameters: INDEX (must be a positive integer) OR "
-        +  PREFIX_PHONE + "PHONE\n"
+        + PREFIX_PHONE + "PHONE\n"
         + "Example 1: " + COMMAND_WORD + " 1\n"
-        //+ "Parameters: " + PREFIX_PHONE + "PHONE\n"
         + "Example 2: " + COMMAND_WORD + " " + PREFIX_PHONE + "98765432";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
