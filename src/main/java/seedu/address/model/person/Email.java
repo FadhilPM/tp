5ee6b0b40b4a00b8485e3f76c09766f79b36c7c5
Cheng Jing <chengjing32@yahoo.com>
2025-03-30 23:45:35 +0800

    Update MESSGAE_CONSTRAINT in Email.java file to shorten the message

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -10,11 +10,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Email {
 
     private static final String SPECIAL_CHARACTERS = "+_.-";
-    public static final String MESSAGE_CONSTRAINTS = "Emails must be in local-part@domain format:\n"
-            + "1. Local-part: Alphanumeric + special characters: " + SPECIAL_CHARACTERS + ", but cannot start/end "
+    public static final String MESSAGE_CONSTRAINTS = "Emails must be in Name@Domain format, for example: user@yahoo.com.\n"
+            + "1. Name: Alphanumeric + special characters: " + SPECIAL_CHARACTERS + ", but cannot start/end "
             + "with a special character.\n"
-            + "2. Domain: Domain Labels separated by periods. Must end with at least 2 character long domain label."
+            + "2. Domain: A domain is made up of domain labels, which are parts separated by periods."
+            +" Domain must end with at least 2 character long domain label."
             + " Labels must start/end with alphanumeric characters, allowing hyphens in between.";
+
     // alphanumeric and special characters
     private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]+"; // alphanumeric characters except underscore
     private static final String LOCAL_PART_REGEX = "^" + ALPHANUMERIC_NO_UNDERSCORE + "([" + SPECIAL_CHARACTERS + "]"
