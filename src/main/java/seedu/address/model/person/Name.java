6a5c60ec942da6ff31d0f4c0a9daafa855c8109e
Cheng Jing <chengjing32@yahoo.com>
2025-03-30 13:57:15 +0800

    Update regex to have a max of 40 characters

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -17,7 +17,7 @@ public class Name {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "^[-a-zA-Z0-9_.][-a-zA-Z0-9_.\\/ ]{0,40}$";
+    public static final String VALIDATION_REGEX = "^[-a-zA-Z0-9_.][-a-zA-Z0-9_.\\/ ]{0,39}$";
 
     public final String fullName;
 
