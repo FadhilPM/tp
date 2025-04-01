c1fb4b26d3543517bead7ad0abcbaa412fe50b55
Fadhil <e0148630@u.nus.edu>
2025-04-01 14:34:54 +0800

    UniquePersonList: improve time efficiency of personsAreUnique

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -3,8 +3,10 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -139,11 +141,11 @@ public class UniquePersonList implements Iterable<Person> {
      * Returns true if {@code persons} contains only unique persons.
      */
     private boolean personsAreUnique(List<Person> persons) {
-        for (int i = 0; i < persons.size() - 1; i++) {
-            for (int j = i + 1; j < persons.size(); j++) {
-                if (persons.get(i).isSamePerson(persons.get(j))) {
-                    return false;
-                }
+        Set<Phone> phoneSet = new HashSet<>();
+        for (Person p: persons) {
+            Phone phoneNumber = p.getPhone();
+            if (!phoneSet.add(phoneNumber)) {
+                return false;
             }
         }
         return true;
