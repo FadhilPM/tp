ec927cd0489220f8cc8925a5402f350c78021867
Fadhil <e0148630@u.nus.edu>
2025-04-01 14:34:29 +0800

    Phone: explicitly require non-null arguments for all methods

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -30,6 +30,7 @@ public class Phone {
      * Returns true if a given string is a valid phone number.
      */
     public static boolean isValidPhone(String test) {
+        requireNonNull(test);
         return test.matches(VALIDATION_REGEX);
     }
 
