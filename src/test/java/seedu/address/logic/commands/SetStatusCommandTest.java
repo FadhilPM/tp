732476e37889d7cb84edd0d7045dc2605ad43b83
broccoli-hater <ihatebroccoli404@gmail.com>
2025-04-07 21:52:40 +0800

    remove unused imports

--- src/test/java/seedu/address/logic/commands/SetStatusCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SetStatusCommandTest.java
@@ -1,18 +1,10 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PROJECT_X;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PROJECT_Y;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -24,7 +16,6 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.EditPersonDescriptor;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.SetStatusDescriptor;
 import seedu.address.testutil.PersonBuilder;
